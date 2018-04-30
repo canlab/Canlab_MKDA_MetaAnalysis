d7864cc593483056fa2e2dfa47c897cb4e74673d
Tor Wager <tor.wager@colorado.edu>
2018-04-30 17:03:07 -0600

    confusion matrix update

--- cluster_multivariate/confusion_matrix.m
+++ cluster_multivariate/confusion_matrix.m
@@ -1,72 +1,104 @@
-function [m,dprime,corr,far,misclass, mprop] = confusion_matrix(true,cla)
-    % [m,dprime,corr,far,missclass, mprop] = confusion_matrix(true,cla)
-    %
-    % calculates a confusion matrix (m, mprop) and d-prime scores
-    % from true class assignments (true) and
-    % estimated class assignments (cla)
-    %
-    % d-prime is actually a-prime, the Z(hr) - Z(far) for classification
-    % corr is hit rate for each class
-    % far is false alarm rate for each class
-    %
-    % also returns vector of which observations were misclassified
-    %
-    % tor wager, 7/08/04
-    % eliminate elements with true class of 0: mod 10/2/06
-    %
-    % m, mprop:
-    % rows are actual (truth) values, columns are predicted (classified)
-    % values, in ascending order of numeric codes
-    % 
-    % m = observation counts
-    % mprop = proportions of observations.
-    %
-    % corr: correct classification rate for true values
-    % True class = 1, 2, etc.
-    % 
-    % far: false alarm rate for true values
-    % % of time a non-class i was classified as class i 
-    %
-    % print output: e.g.,
-    % print_matrix(m, {'Pred. Innocent' 'Pred. Guilty'}, {'Actual Innocent' 'Actual Guilty'});
+function [m,aprime,corr,far,misclass, mprop, statstable] = confusion_matrix(true,cla)
+% [m,aprime,corr,far,missclass, mprop, statstable] = confusion_matrix(true,cla)
+%
+% calculates a confusion matrix (m, mprop) and a-prime scores (AUC
+% effect size)
+%
+% from true class assignments (true) and
+% estimated class assignments (cla)
+%
+% a-prime is Z(hr) - Z(far) for classification, like d-prime
+% - corr/far of 0.0/1.0 are adjusted to 0.005/.995 to avoid unrealistic
+% aprime values. Max a-prime is 5.15.
+%
+% corr is sensitivity, hit rate for each class, same as accuracy
+% far is false alarm rate for each class. specificity = 1-far
+%
+% also returns vector of which observations were misclassified
+%
+% m, mprop:
+% rows are actual (truth) values, columns are predicted (classified)
+% values, in ascending order of numeric codes
+% Accuracy is diag(mprop), same as sensitivity
+%
+% m = observation counts
+% mprop = proportions of observations.
+%
+% corr: correct classification rate for true values
+% True class = 1, 2, etc.
+%
+% far: false alarm rate for true values
+% % of time a non-class i was classified as class i
+%
+% Other quantities in statstable output:
+% ppv: Positive preditive value
+% cases: Num cases in each condition, sum(m, 2)
+% freq: Frequency of each condition, sum(m, 2) ./ sum(m(:))
+%
+% print output: e.g.,
+% print_matrix(m, {'Pred. Innocent' 'Pred. Guilty'}, {'Actual Innocent' 'Actual Guilty'});
 %
 % Output table:
-% mplus = [m corr']; 
+% mplus = [m corr'];
 % total_class_rate = 1 - (sum(missclass) ./ length(missclass));
 % mplus = [mplus; [far total_class_rate]];
 % print_matrix(mplus, {'Pred. Innocent' 'Pred. Guilty' 'Correct Class Rate'}, {'Actual Innocent' 'Actual Guilty' 'False Alarm Rate'});
 
-    wh = (true ~= 0);
-    n = length(true);
-    misclass = false(n,1);
+% Programmers' notes:
+%
+% tor wager, 7/08/04
+% eliminate elements with true class of 0: mod 10/2/06
+%
+% tor : 4/2018.  corr/far of 0.0/1.0 are adjusted to 0.005/.995 to avoid unrealistic
+% aprime values.
 
-    true = true(wh);
-    cla = cla(wh);
+wh = (true ~= 0);
+n = length(true);
+misclass = false(n,1);
 
-    u = unique(true);
+true = true(wh);
+cla = cla(wh);
+
+u = unique(true);
+
+for i = 1:length(u)
     
-    for i = 1:length(u)
-
-        n(i) = sum(true == u(i));
+    n(i) = sum(true == u(i));
+    
+    corr(i) = sum(true == u(i) & cla == u(i)) ./ sum(true == u(i));
+    far(i) = sum(true ~= u(i) & cla == u(i)) ./ sum(true ~= u(i));
+    
+    % rows are actual (truth) values, columns are predicted (classified)
+    % values
+    for j = 1:length(u)
+        m(i,j) = sum(true == u(i) & cla == u(j));
         
-        corr(i) = sum(true == u(i) & cla == u(i)) ./ sum(true == u(i));
-        far(i) = sum(true ~= u(i) & cla == u(i)) ./ sum(true ~= u(i));
-
-        % rows are actual (truth) values, columns are predicted (classified)
-        % values
-        for j = 1:length(u)
-            m(i,j) = sum(true == u(i) & cla == u(j));
-            
-            mprop(i,j) = m(i,j) ./ n(i);
-        end
-
+        mprop(i,j) = m(i,j) ./ n(i);
     end
+    
+end
 
-    dprime = norminv(corr) - norminv(far);
+% ppv: Positive preditive value
+ppv = corr ./ (corr + far);
 
-    % avoid Inf values due to perfect solutions
-    dprime(abs(dprime) > 10) = sign(dprime(abs(dprime) > 10)) * 4;
+    
 
-    misclass(wh) = (true - cla) ~= 0;
+% a-prime: avoid perfect scores
+corr_for_aprime = corr;
+corr_for_aprime(corr_for_aprime == 0) = .005;
+corr_for_aprime(corr_for_aprime == 1) = .995;
 
-    return
+far_for_aprime = far;
+far_for_aprime(far_for_aprime == 0) = .005;
+far_for_aprime(far_for_aprime == 1) = .995;
+
+aprime = norminv(corr_for_aprime) - norminv(far_for_aprime);
+
+% avoid Inf values due to perfect solutions
+% aprime(abs(aprime) > 10) = sign(aprime(abs(aprime) > 10)) * 4;
+
+misclass(wh) = (true - cla) ~= 0;
+
+statstable = table(sum(m, 2), sum(m, 2) ./ sum(m(:)), corr', (1-far)', ppv', aprime', 'VariableNames', {'Freq' 'Perc_cases' 'Sens' 'Spec' 'PPV' 'Aprime'});
+
+end
