5c47aa1f76b29213918701511eb56baa245769f0
Tor Wager <torwager@gmail.com>
2021-10-27 08:50:23 -0400

    Update confusion_matrix.m

--- cluster_multivariate/confusion_matrix.m
+++ cluster_multivariate/confusion_matrix.m
@@ -2,33 +2,37 @@ function [m,aprime,corr,far,misclass, mprop, statstable] = confusion_matrix(true
 % [m,aprime,corr,far,missclass, mprop, statstable] = confusion_matrix(true,cla)
 %
 % calculates a confusion matrix (m, mprop) and a-prime scores (AUC
-% effect size)
+% effect size) from true class assignments (true) and estimated class assignments (cla)
 %
-% from true class assignments (true) and
-% estimated class assignments (cla)
+% Notes: 
+% - Do not enter values of 0 for true vector. These are interpreted as missing cases.
+% - Will sort class order from smallest values to largest
 %
-% a-prime is Z(hr) - Z(far) for classification, like d-prime
-% - corr/far of 0.0/1.0 are adjusted to 0.005/.995 to avoid unrealistic
-% aprime values. Max a-prime is 5.15.
-%
-% corr is sensitivity, hit rate for each class, same as accuracy
-% far is false alarm rate for each class. specificity = 1-far
-%
-% also returns vector of which observations were misclassified
-%
+% Outputs:
 % m, mprop:
-% rows are actual (truth) values, columns are predicted (classified)
-% values, in ascending order of numeric codes
-% Accuracy is diag(mprop), same as sensitivity
+%   Confusion matrix with true x estimated classes
+%   rows are actual (truth) values, columns are predicted (classified)
+%   values, in ascending order of numeric codes
+%   m = observation counts
+%   mprop = proportions of observations.
 %
-% m = observation counts
-% mprop = proportions of observations.
+% Accuracy is diag(mprop), which is the same as sensitivity
+% Balanced accuracy is mean(diag(mprop))
 %
-% corr: correct classification rate for true values
-% True class = 1, 2, etc.
+% a-prime: A measure of effect size
+%   Z(hr) - Z(far) for classification, like d-prime
+%   Note: corr and far of 0.0/1.0 are adjusted to 0.005/.995 to avoid unrealistic
+%   aprime values. Max a-prime is 5.15.
+%
+% corr: sensitivity, hit rate for each class, same as accuracy
+%   correct classification rate for true exemplars of each class
+%   True class = 1, 2, etc.
 %
 % far: false alarm rate for true values
-% % of time a non-class i was classified as class i
+%   proportion of time a non-class i was classified as class i
+%   far is false alarm rate for each class. specificity = 1-far
+%
+% misclass: vector of which observations were misclassified
 %
 % Other quantities in statstable output:
 % ppv: Positive preditive value
@@ -43,7 +47,11 @@ function [m,aprime,corr,far,misclass, mprop, statstable] = confusion_matrix(true
 % total_class_rate = 1 - (sum(missclass) ./ length(missclass));
 % mplus = [mplus; [far total_class_rate]];
 % print_matrix(mplus, {'Pred. Innocent' 'Pred. Guilty' 'Correct Class Rate'}, {'Actual Innocent' 'Actual Guilty' 'False Alarm Rate'});
-
+% 
+% To create a table object with output:
+% mytable = array2table(mprop, 'VariableNames', {'Pred Cog' 'Pred Pain'}, 'RowNames', {'Actual Cog' 'Actual Pain'});
+% disp(mytable)
+%
 % Programmers' notes:
 %
 % tor wager, 7/08/04
@@ -60,6 +68,11 @@ true = true(wh);
 cla = cla(wh);
 
 u = unique(true);
+
+if length(u) == 1
+    warning('Do not enter values of 0 for true vector. These are interpreted as missing cases.')
+    error('Only one class detected. Check inputs and assign non-zero integer labels to valid class exemplars.');
+end
 
 for i = 1:length(u)
     
