c77c20b96516e22890b604427c555d361fca0da0
Tor Wager <torwager@gmail.com>
2024-01-25 16:17:16 -0500

    Update string2indicator.m

--- densityUtility3/Support_functions/string2indicator.m
+++ densityUtility3/Support_functions/string2indicator.m
@@ -7,21 +7,33 @@ function [indic, nms, condf] = string2indicator(str,varargin)
 % Examples:
 %[indic,nms] = string2indicator(CL{1}(1).valence);
 %[indic,nms] = string2indicator(CL{1}(1).valence,{'neg' 'pos'});
+%
+% Note: This function changed 11/1/2023 to return names in stable order
+% (the order which they appear in the original str input).
+% Also, previously, the function would accept a non-cell string matrix but
+% return potentially incorrect output; now enforces cell array
 
-if length(varargin) > 0
-    nms = varargin{1};
-else
-    nms = unique(str);
+
+if ~iscell(str) 
+    str = cellstr(str);
 end
 
-if ~iscell(nms), for i = 1:length(nms), nms2{i} = nms(i);,end, nms = nms2;,end
-        
-nms(strcmp(nms,'NaN')) = [];
+if ~isempty(varargin)
+    nms = varargin{1};
+else
+    nms = unique(str, 'stable');  % tor edited 11/1/2023, 'rows' would fix edge case with non-cell string matrix, but now enforce cell anyway 
+end
 
-indic = zeros(length(str),1);
+if ~iscell(nms) %, for i = 1:length(nms), nms2{i} = nms(i); end, nms = nms2; end  % this was returning only the first character of a string matrix
+    nms = cellstr(nms);
+end
+
+nms(strcmp(nms, 'NaN')) = [];
+
+indic = zeros(length(str), 1);
 
 for k = 1:length(nms)
-    indic(:,k) = strcmp(str,nms{k});
+    indic(:,k) = strcmp(str, nms{k});
 end
 
 if size(nms,1) > size(nms,2)
@@ -33,4 +45,4 @@ if nargout > 2
 	condf = indic2condf(indic);
 end
 
-return
+end
